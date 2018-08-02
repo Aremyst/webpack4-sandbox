7eefe824565985dd27567581e43cf83d294eb5d7
Dmitriy Naimark <dmitrynaimark@gmail.com>
2018-08-02 11:52:21 +0300

    > npm install --save-dev style-loader css-loader
    Edit "webpack.config.js", so that .css files are using 2 loaders.
    
    Note:
        This enables you to import './style.css' into the file that depends on that styling.
        Now, when that module is run, a <style> tag with the stringified css will be inserted into the <head> of your html file.
    
    Create "style.css" and import it from "index.js".

--- webpack.js.org/2018-08-02 - Asset Management - Loading CSS, images, fonts, data, global assets/webpack-demo/src/index.js
+++ webpack.js.org/2018-08-02 - Asset Management - Loading CSS, images, fonts, data, global assets/webpack-demo/src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
     var element = document.createElement('div');
     
     // Lodash, now imported by current index.js script(see above).
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
     
     return element;
 }
